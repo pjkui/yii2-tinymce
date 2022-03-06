c8892de4d818703f16f1654d227b4f0467350cf6
Quinn Pan <pjkui@qq.com>
2022-03-06 23:45:51 +0800

    add tinymce

--- src/Asset.php
+++ src/Asset.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace pjkui\Yii2Tinymce;
 
 class Asset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@bower/tinymce';
+    public $sourcePath = '@vendor/tinymce/tinymce';
 
     public $js = [
         'tinymce.min.js',
